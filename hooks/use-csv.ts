e350fa19e6307cdf461696ee5914209a472d88aa
Ali Kristoffer Njie <ali.k.njie@gmail.com>
2022-02-19 15:33:50 +0100

    chore: refinements

--- hooks/use-csv.ts
+++ hooks/use-csv.ts
@@ -1,14 +1,16 @@
+import { range } from "../utils/range"
+
 const SEPARATOR_REGEX = /(?!\B"[^"]*)[	,;](?![^"]*"\B)/
 const QUOTED_REGEX = /(?<!.)(\B")|"\B(?!.)/g
 
-export function useCSV(csvString: string, hasHeaders = true) {
+export function useCSV(csvString: string, hasHeaders: boolean) {
   // Regex matches characters "  ", "," or ";" unless they're enclosed by quotes
   const rawRows = csvString
     // Handles removal of last newline if there are any
     .replace(/\n$/, "")
     .split("\n")
 
-  const rows = rawRows.reduce<string[][]>((acc, row) => {
+  const rows = rawRows.map((row) => {
     const data = row.split(SEPARATOR_REGEX)
 
     // Handles removal of quotes within strings
@@ -19,16 +21,12 @@ export function useCSV(csvString: string, hasHeaders = true) {
       }
     })
 
-    acc.push(data)
-
-    return acc
+    return data
   }, [])
 
   const headers = hasHeaders
     ? rows[0]
-    : Array(rows[0].length)
-        .fill(null)
-        .map((_, i) => `Column ${i + 1}`)
+    : range(rows[0].length).map((n) => `Column ${n}`)
 
   return { rows: hasHeaders ? rows.splice(1) : rows, headers }
 }
